0628b66f205d7f79a7feea04f1a28ec724eb625a
ndkcha <ndkcha@gmail.com>
2017-10-06 19:29:43 -0400

    Renamed UDP data holder

--- src/schema/UdpPacket.java
+++ src/schema/UdpPacket.java
@@ -3,12 +3,12 @@ package schema;
 import java.io.Serializable;
 import java.util.HashMap;
 
-public class AuthUdpPacket implements Serializable {
+public class UdpPacket implements Serializable {
     private static final long serialVersionUID = 1L;
     public int operationName;
     public HashMap<String, Object> body;
 
-    public AuthUdpPacket(int operationName, HashMap<String, Object> body) {
+    public UdpPacket(int operationName, HashMap<String, Object> body) {
         this.operationName = operationName;
         this.body = body;
     }
